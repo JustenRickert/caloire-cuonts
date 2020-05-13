da537c7e15694945756705d5574b9bdff21367b8
jsasz <JustenRickert@gmail.com>
2020-05-12 20:49:32 -0500

    Init the real real y'know y'know

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { v4 as uuid } from 'uuid'
+import { format } from 'date-fns'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function saveEntries(data) {
+  return localStorage.setItem('data', JSON.stringify(data))
 }
 
-export default App;
+function readEntries() {
+  return JSON.parse(localStorage.getItem('data'))
+}
+
+function Input({ onSubmit }) {
+  const [addingNew, setAddingNew] = useState(false)
+  const [date, setDate] = useState(format(new Date(), 'yyyy-MM-dd'))
+  const handleSumbit = e => {
+    e.preventDefault()
+    const { food, calories } = e.currentTarget.elements
+    const convertedDate = new Date(date)
+    convertedDate.setMinutes(
+      convertedDate.getMinutes() + convertedDate.getTimezoneOffset()
+    )
+    onSubmit({
+      food: food.value,
+      calories: calories.value,
+      date: convertedDate,
+    })
+    setAddingNew(false)
+  }
+  return !addingNew ? (
+    <button onClick={() => setAddingNew(true)}>Add new</button>
+  ) : (
+    <form onSubmit={handleSumbit}>
+      <div>
+        <label htmlFor="food">Food</label>
+        <input required type="text" id="food" name="food" />
+      </div>
+      <br />
+      <div>
+        <label htmlFor="calories">Calorie Count</label>
+        <input required type="number" id="calories" name="calories" />
+      </div>
+      <br />
+      <label htmlFor="date">Date</label>
+      <input
+        required
+        type="date"
+        id="date"
+        name="date"
+        onChange={e => setDate(e.target.value)}
+        value={date}
+      />
+      <br />
+      <button type="submit">submit</button>
+    </form>
+  )
+}
+
+const bucket = (xs, toKey) =>
+  xs.reduce(
+    (acc, x) => ({
+      ...acc,
+      [toKey(x)]: (acc[toKey(x)] || []).concat(x),
+    }),
+    {}
+  )
+
+function App() {
+  const [entries, setEntries] = useState(readEntries() || [])
+  const deleteEntry = id => {
+    setEntries(entries => entries.filter(e => e.id !== id))
+  }
+  const addEntry = entry => {
+    setEntries(entries => entries.concat(Object.assign(entry, { id: uuid() })))
+  }
+  useEffect(() => {
+    saveEntries(entries)
+  }, [entries])
+  const entriesByDate = bucket(entries, entry => entry.date)
+  return (
+    <div className="App">
+      {Object.entries(entriesByDate).map(([date, entries]) => (
+        <React.Fragment key={date}>
+          <h4>{new Date(date).toLocaleDateString()}</h4>
+          <ul>
+            {entries.map(({ food, calories, id }) => (
+              <li key={id}>
+                {food} ({calories}){' '}
+                <button onClick={() => deleteEntry(id)}>X</button>
+              </li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ))}
+      <Input onSubmit={addEntry} />
+    </div>
+  )
+}
+
+export default App
